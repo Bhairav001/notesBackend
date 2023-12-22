0c8b940cf53e19dfcb727582528f5bbcefaeb8ec
Bhairav <bhairavgotam066@gmail.com>
2023-12-22 13:26:08 +0530

    added update one

--- routes/crud.route.js
+++ routes/crud.route.js
@@ -2,9 +2,9 @@ const express = require("express");
 
 const { crudModel } = require("../model/crud.model");
 
-const path = require('path');
+// const path = require('path');
 
-const multer = require("multer");
+// const multer = require("multer");
 
 
 
@@ -61,24 +61,24 @@ crudRoutes.delete("/delete/:id",async(req,res)=>{
 })
 
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        const uploadPath = path.join(__dirname,"public", "images");
-        console.log("uploadPath",uploadPath)
-        cb(null, uploadPath);
-    },
-    filename: function (req, file, cb) {
-        cb(null, `${Date.now()}_${file.originalname}`);
-    }
-});
+// const storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//         const uploadPath = path.join(__dirname,"public", "images");
+//         console.log("uploadPath",uploadPath)
+//         cb(null, uploadPath);
+//     },
+//     filename: function (req, file, cb) {
+//         cb(null, `${Date.now()}_${file.originalname}`);
+//     }
+// });
 
-const upload = multer({storage:storage})
-console.log("upload",upload)
-crudRoutes.post("/upload",upload.single("file"),(req,res)=>{
-    console.log(req.body);
-    console.log(req.file);
-    res.send("uploaded file successfully!");
-})
+// const upload = multer({storage:storage})
+// console.log("upload",upload)
+// crudRoutes.post("/upload",upload.single("file"),(req,res)=>{
+//     console.log(req.body);
+//     console.log(req.file);
+//     res.send("uploaded file successfully!");
+// })
 
 module.exports={
     crudRoutes
